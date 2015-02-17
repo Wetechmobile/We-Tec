2d7b6e5a6d8f396c54ee3a4674ff7be4948df537
Wetechmoiblie <dtrade93@gmail.com>
2015-02-17 13:04:30 -0500

    Update 2

---
+++ assets/2-nd-pg-ani/2 nd pg ani_edgeActions.js
@@ -0,0 +1,8 @@
+
+(function($,Edge,compId){var Composition=Edge.Composition,Symbol=Edge.Symbol;
+//Edge symbol: 'stage'
+(function(symbolName){Symbol.bindElementAction(compId,symbolName,"${_atlanta_golf_icon}","click",function(sym,e){window.open("golf.html","_blank");});
+//Edge binding end
+})("stage");
+//Edge symbol end:'stage'
+})(jQuery,AdobeEdge,"EDGE-206857363");
